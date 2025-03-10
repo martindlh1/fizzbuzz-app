4a0eb3dbfdfe8f1566112993431dc0d817974421
martin <martin.delahousse@epitech.eu>
2025-03-10 14:22:19 +0100

    [ADD] basic web app

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('fizzbuzz-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const fizz_nb = document.getElementById('fizz_nb').value;
+    const buzz_nb = document.getElementById('buzz_nb').value;
+    const start = document.getElementById('start').value;
+    const end = document.getElementById('end').value;
+
+    const data = JSON.stringify({ fizz_nb, buzz_nb, start, end });
+    console.log(data);
+
+    fetch('http://localhost:8000/fizzbuzz', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: data,
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('result').innerText = data.join(', ');
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
